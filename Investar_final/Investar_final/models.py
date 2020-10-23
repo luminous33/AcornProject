6ecdf0dda30c23efe501153f0a1e76838389a220
Ubuntu <ubuntu@ip-172-31-42-154.ap-northeast-2.compute.internal>
2020-10-23 01:53:16 +0000

    밍슈까먹지마><

--- Investar_final/Investar_final/models.py
+++ Investar_final/Investar_final/models.py
@@ -5,6 +5,7 @@
 #   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
+"""
 from django.db import models
 
 
@@ -158,3 +159,47 @@ class DjangoSession(models.Model):
     class Meta:
         managed = False
         db_table = 'django_session'
+        """
+from django.db import models
+
+
+class Merge(models.Model):
+    company = models.CharField(max_length=40, blank=True, null=True)
+    code = models.CharField(max_length=20)
+    date = models.DateField()
+    open = models.BigIntegerField(blank=True, null=True)
+    high = models.BigIntegerField(blank=True, null=True)
+    low = models.BigIntegerField(blank=True, null=True)
+    close = models.BigIntegerField(blank=True, null=True)
+    diff = models.BigIntegerField(blank=True, null=True)
+    volume = models.BigIntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'MERGE'
+
+
+class CompanyInfo(models.Model):
+    code = models.CharField(primary_key=True, max_length=20)
+    company = models.CharField(max_length=40, blank=True, null=True)
+    last_update = models.DateField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'company_info'
+
+
+class DailyPrice(models.Model):
+    code = models.CharField(primary_key=True, max_length=20)
+    date = models.DateField()
+    open = models.BigIntegerField(blank=True, null=True)
+    high = models.BigIntegerField(blank=True, null=True)
+    low = models.BigIntegerField(blank=True, null=True)
+    close = models.BigIntegerField(blank=True, null=True)
+    diff = models.BigIntegerField(blank=True, null=True)
+    volume = models.BigIntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'daily_price'
+        unique_together = (('code', 'date'),)
