400b0f1f5d16f42740888697aec234d2a81d0b0d
Ubuntu <ubuntu@ip-172-31-42-154.ap-northeast-2.compute.internal>
2020-11-02 04:14:55 +0000

    create txt

--- Investar_final/Investar_final/wsgi.py
+++ Investar_final/Investar_final/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for Investar_final project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
-
+sys.path.append('/srv/AcornProject/Investar_final')
+os.environ.setdefault('PYTHON_EGG_CACHE', '/srv/AcornProject/Investar_final/egg_cache')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Investar_final.settings')
-
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
